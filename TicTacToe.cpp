cc59bb4f208ca6d24b637a35d910e3ed7d838960
Sage Garrett <saga1742@colorado.edu>
2020-02-10 20:45:21 -0700

    added CreateBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,1 +1,17 @@
-//function def here
+#include <iostream>
+
+std::string** CreateBoard() {
+	std::string** board = new std::string*[3];
+	for(int i = 0; i < 3; i++) {
+		board[i] = new std::string[3];
+		for(int j= 0; j < 3; j++) {
+			board[i][j] = "";
+		}
+	}
+	return board;
+}
+
+
+int main() {
+	std::string** board = CreateBoard();
+}
